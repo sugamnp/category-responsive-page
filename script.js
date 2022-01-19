0c12e9b450be7d5d42d8e88d79552fab3f5c8ee3
burr1to <singhsanskar240@gmail.com>
2022-01-19 19:14:30 +0545

    still working

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 var photosource;
 var previewpage;
 
-
-function showPreview(event){
-  if(event.target.files.length > 0){
+function showPreview(event) {
+  if (event.target.files.length > 0) {
     var src = URL.createObjectURL(event.target.files[0]);
-    var preview = document.getElementById("image-preview__image");
+    var preview = document.querySelector("img");
     previewpage = preview;
     preview.src = src;
     preview.style.display = "block";
     photosource = src;
   }
+}
+
+/*function showAR() {
+  var i = document.querySelector("img");
+  i.classList.remove("def");
+  i.classList.toggle("sub-cat1");
+}
+
+*/
+const buttonShow = document.querySelector(".btn1");
+
+buttonShow.addEventListener("click", () => {
+  let selectAll = document.querySelectorAll("input[type = checkbox]");
+
+  for (var i = 0; i < selectAll.length; i++) {
+    selectAll[i].checked = true;
+  }
+});
+
+const buttonHide = document.querySelector(".btn2");
+
+buttonHide.addEventListener("click", () => {
+  let deselectAll = document.querySelectorAll("input[type = checkbox]");
+
+  for (var i = 0; i < deselectAll.length; i++) {
+    deselectAll[i].checked = false;
+  }
+});
+
+function catEn() {
+  const categoryOne = document.querySelectorAll(".cat1");
+  console.log(categoryOne);
+  let checkState = categoryOne[0].checked;
+  categoryOne[1].checked = true;
+  categoryOne[2].checked = true;
 }
